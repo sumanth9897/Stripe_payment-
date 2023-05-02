07137ec8723e365a0253e5618dd94ebc6659841a
sumanth9897 <sarantejasumanth.m20@iiits.in>
2023-05-02 15:45:06 +0530

    final commit

---
+++ Backend/products.js
@@ -0,0 +1,31 @@
+const products = [
+  {
+    id: 1,
+    name: "iPhone 12 Pro",
+    brand: "Apple",
+    desc: "6.1-inch display",
+    price: 999,
+    image:
+      "https://res.cloudinary.com/dnn1dsphe/image/upload/v1679583829/refurb-iphone-12-pro-blue-2020_vlsjpk.jpg",
+  },
+  {
+    id: 2,
+    name: "iPhone 12",
+    brand: "Apple",
+    desc: "5.4-inch mini display",
+    price: 699,
+    image:
+      "https://res.cloudinary.com/dnn1dsphe/image/upload/v1679583882/refurb-iphone-12-mini-black-2020_p2vnhh.jpg",
+  },
+  {
+    id: 3,
+    name: "Galaxy S",
+    brand: "Samsung",
+    desc: "6.5-inch display",
+    price: 399,
+    image:
+      "https://res.cloudinary.com/dnn1dsphe/image/upload/v1679583950/levant-galaxy-s21-ultra-5g-g988-371058-sm-g998bzkgmea-thumb-368334301_fttloa.png",
+  },
+];
+
+module.exports = products;
